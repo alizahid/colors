5f15fbbac0dad4c2f76994430ed4000a2a8b43c5
Ali Zahid <ali.zahid@live.com>
2017-03-28 12:41:18 +0500

    Refactoring

--- assets/js/themes.js
+++ assets/js/themes.js
@@ -1,100 +1,98 @@
-var themes = [
-    {
-		name: 'Drop',
-        link: 'http://dropcolorpicker.com',
-        colors: 'f0665b,f0665b,51c97f,59e5c5,4bbde8,417cf9,6190b8,9777d8'.split(','),
-        added: new Date(2017, 2, 28),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-		name: 'Somewhere from Dribbble',
-        link: 'https://dribbble.com/alizahid',
-        colors: 'fb5a5b,59565c,fccf87,33adc0,84d177,cc9367,dde8e9,e8e8dc'.split(','),
-        added: new Date(2017, 2, 27),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-		name: 'Facebook Messenger',
-        link: 'https://www.messenger.com',
-        colors: '1787fb,689aca,31cef3,4bbec6,27cd2b,6ab76b,fdc22d,fd7e37,d3a88e,f73f51,e48687,fd5fa1,d597ba,764ffb,a696c5'.split(','),
-        added: new Date(2017, 1, 6),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-        name: 'iOS',
-        link: 'https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/',
-        colors: 'ff3b30,ff9500,ffcc00,4cd964,5ac8fa,007aff,5856d6,ff2d55'.split(','),
-        added: new Date(2017, 1, 6),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-        name: 'SkyAdventures',
-        link: 'https://dribbble.com/shots/2066288-SkyAdventures-Iconset-Palette',
-        colors: '0075ff,00b7d6,02d2b0,e79f00,ff7143'.split(','),
-        added: new Date(2016, 2, 6),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-        name: 'Translateur',
-        link: 'https://translateur.co',
-        colors: 'd54400,949c27,367c86,3a9bc9,edb630,ee8360,367c86,ce4e92,a58162,8080b4,4d8300,9f0b35,633'.split(','),
-        added: new Date(2015, 2, 6),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-        name: 'Grays',
-        colors: 'f6f7f8,ecf0f1,e6e9e9,bdc3c7'.split(','),
-        added: new Date(2014, 1, 13),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-        name: 'Stacking Order',
-        colors: 'b377bf,8277bf,5081c2,54c3ac,a5c24d,e8953e,e74139'.split(','),
-        added: new Date(2014, 8, 30),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-        name: 'Flat UI',
-        link: 'https://designmodo.github.io/Flat-UI',
-        colors: '1abc9c,16a085,2ecc71,27ae60,3498db,2980b9,9b59b6,8e44ad,34495e,2c3e50,f1c40f,f39c12,e67e22,d35400,e74c3c,c0392b,ecf0f1,bdc3c7,95a5a6,7f8c8d'.split(','),
-        added: new Date(2014, 3, 29),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-        name: 'Origin of Common UI Symbols',
-        link: 'https://readymag.com/shuffle/ui-symbols/',
-        colors: '28a0a0,3a61e0,61523d,d37c39,8e8485,1e7c7f,228446,995d98,34495e'.split(','),
-        added: new Date(2014, 3, 28),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
-    }, {
-        name: 'Captain Icon',
-        link: 'http://mariodelvalle.github.io/CaptainIconWeb/',
-        colors: 'f6e5d0,e6c7b9,d3807b,c5e3c7,bfe1dd,8c4a62,594255,726873,bfb2a3,c0c0c0'.split(','),
-        added: new Date(2014, 4, 2),
-        addedBy: {
-            name: 'Ali Zahid',
-            link: 'http://alizahid.net'
-        }
+var themes = [{
+    name: 'Drop',
+    link: 'http://dropcolorpicker.com',
+    colors: 'f0665b,f0665b,51c97f,59e5c5,4bbde8,417cf9,6190b8,9777d8'.split(','),
+    added: new Date(2017, 2, 28),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
     }
-]
+}, {
+    name: 'Somewhere from Dribbble',
+    link: 'https://dribbble.com/alizahid',
+    colors: 'fb5a5b,59565c,fccf87,33adc0,84d177,cc9367,dde8e9,e8e8dc'.split(','),
+    added: new Date(2017, 2, 27),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}, {
+    name: 'Facebook Messenger',
+    link: 'https://www.messenger.com',
+    colors: '1787fb,689aca,31cef3,4bbec6,27cd2b,6ab76b,fdc22d,fd7e37,d3a88e,f73f51,e48687,fd5fa1,d597ba,764ffb,a696c5'.split(','),
+    added: new Date(2017, 1, 6),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}, {
+    name: 'iOS',
+    link: 'https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/',
+    colors: 'ff3b30,ff9500,ffcc00,4cd964,5ac8fa,007aff,5856d6,ff2d55'.split(','),
+    added: new Date(2017, 1, 6),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}, {
+    name: 'SkyAdventures',
+    link: 'https://dribbble.com/shots/2066288-SkyAdventures-Iconset-Palette',
+    colors: '0075ff,00b7d6,02d2b0,e79f00,ff7143'.split(','),
+    added: new Date(2016, 2, 6),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}, {
+    name: 'Translateur',
+    link: 'https://translateur.co',
+    colors: 'd54400,949c27,367c86,3a9bc9,edb630,ee8360,367c86,ce4e92,a58162,8080b4,4d8300,9f0b35,633'.split(','),
+    added: new Date(2015, 2, 6),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}, {
+    name: 'Grays',
+    colors: 'f6f7f8,ecf0f1,e6e9e9,bdc3c7'.split(','),
+    added: new Date(2014, 1, 13),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}, {
+    name: 'Stacking Order',
+    colors: 'b377bf,8277bf,5081c2,54c3ac,a5c24d,e8953e,e74139'.split(','),
+    added: new Date(2014, 8, 30),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}, {
+    name: 'Flat UI',
+    link: 'https://designmodo.github.io/Flat-UI',
+    colors: '1abc9c,16a085,2ecc71,27ae60,3498db,2980b9,9b59b6,8e44ad,34495e,2c3e50,f1c40f,f39c12,e67e22,d35400,e74c3c,c0392b,ecf0f1,bdc3c7,95a5a6,7f8c8d'.split(','),
+    added: new Date(2014, 3, 29),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}, {
+    name: 'Origin of Common UI Symbols',
+    link: 'https://readymag.com/shuffle/ui-symbols/',
+    colors: '28a0a0,3a61e0,61523d,d37c39,8e8485,1e7c7f,228446,995d98,34495e'.split(','),
+    added: new Date(2014, 3, 28),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}, {
+    name: 'Captain Icon',
+    link: 'http://mariodelvalle.github.io/CaptainIconWeb/',
+    colors: 'f6e5d0,e6c7b9,d3807b,c5e3c7,bfe1dd,8c4a62,594255,726873,bfb2a3,c0c0c0'.split(','),
+    added: new Date(2014, 4, 2),
+    addedBy: {
+        name: 'Ali Zahid',
+        link: 'http://alizahid.net'
+    }
+}]
